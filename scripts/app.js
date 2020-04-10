0279a9ddc17955556c2a3a8738cc5abc6db7b57d
Ben Roberts <benroberts67@hotmail.co.uk>
2020-04-10 15:58:30 +0100

    random ship placement complete, the board now receives 5 ships in random locations. They tend to bunch up, see if there is a simple solution to make them more separated. Next task is to format the end of each ship to be different to the middle.

--- scripts/app.js
+++ scripts/app.js
@@ -24,44 +24,164 @@ function init(){
   
   createCells()
 
-
+  
 
   const carrier = {
-    length: 8,
+    length: 5,
+    location: [],
+    hit: []
+  }
+
+  const battleship = {
+    length: 4,
+    location: [],
+    hit: []
+  }
+  const destroyer = {
+    length: 3,
+    location: [],
+    hit: []
+  }
+  const submarine = {
+    length: 3,
+    location: [],
+    hit: []
+  }
+  const patrolboat = {
+    length: 2,
     location: [],
     hit: []
   }
 
 
 
+  const allNums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
-  function createCarrier (ship){
+  const checkNums = [21,24,79]
+  let numberChecked = []
 
-    if (Math.round(Math.random()) > 0){
-      const shipRef = horizontalShip(ship.length)
-      for (let i = 0; i < ship.length; i++){
-        carrier.location.push(shipRef + i)
-      }
-    } else {
-      let shipRef = verticalShip(ship.length)
-      for (let i = 0; i < ship.length; i++){
-        ship.location.push(shipRef += 10)
-      }
-    }
-
-    console.log(carrier.location)
-
-    carrier.location.forEach(location => {
-      cells[location].classList.add('ship')
+  function numCheck (){ 
+    numberChecked = allNums.some(num => {
+      return checkNums.includes(num)
     })
-  
-
   }
 
+  // numCheck()
+  // console.log(numberChecked)
+
+  // console.log(someAreNums)
+  const allShipLocations = []
+
+
+
+  function createShip (ship){
+
+    return (Math.round(Math.random()) > 0) ? createHorizontalShip(ship) : createVerticalShip(ship)
+      
+    
+    function createHorizontalShip (ship){
+      const shipRef = horizontalShip(ship.length)
+      let tempShip = []
+      for (let i = 0; i < ship.length; i++){
+        tempShip.push(shipRef + i)
+      }
+      if (allShipLocations.some(num => {
+        return tempShip.includes(num)
+      })) {
+        tempShip = []
+        createHorizontalShip(ship)
+      } else {
+        tempShip.forEach(location => {
+          ship.location.push(location)
+          allShipLocations.push(location)
+        })
+      }
+    } 
+
+
+
+    function createVerticalShip (ship) {
+      let shipRef = verticalShip(ship.length)
+      let tempShip = []
+      for (let i = 0; i < ship.length; i++){
+        tempShip.push(shipRef += 10)
+      }
+      if (allShipLocations.some(num => {
+        return tempShip.includes(num)
+        
+      })){
+        tempShip = []
+        createVerticalShip(ship)
+      } else {
+        tempShip.forEach(location => {
+          ship.location.push(location)
+          allShipLocations.push(location)
+        })
+      }
+    }
+  }
+  
+  // console.log(ship.location)
+
+ 
+
+  // }
+  
+  // const allCompShips = carrier.location.concat(battleship.location,destroyer.location,submarine.location,patrolboat.location)
 
   
   
-  createCarrier(carrier)
+  createShip(carrier)
+  // console.log(allCompShips)
+  // console.log(carrier.location)
+  console.log(allShipLocations)
+  
+  createShip(battleship)
+  // console.log(allCompShips)
+  // console.log(battleship.location)
+  console.log(allShipLocations)
+  
+  createShip(destroyer)
+  // console.log(allCompShips)
+  // console.log(destroyer.location)
+  console.log(allShipLocations)
+  
+  createShip(submarine)
+  // console.log(allCompShips)
+  // console.log(submarine.location)
+  console.log(allShipLocations)
+  
+  createShip(patrolboat)
+  // console.log(allCompShips)
+  // console.log(patrolboat.location)
+  console.log(allShipLocations)
+  
+
+  
+  
+  carrier.location.forEach(location => {
+    cells[location].classList.add('ship')
+  })
+
+  // carrier.location[0].classList.add('ship-end')
+  
+  battleship.location.forEach(location => {
+    cells[location].classList.add('ship')
+  })
+  
+  destroyer.location.forEach(location => {
+    cells[location].classList.add('ship')
+  })
+  
+  submarine.location.forEach(location => {
+    cells[location].classList.add('ship')
+  })
+  
+  patrolboat.location.forEach(location => {
+    cells[location].classList.add('ship')
+  })
+  
+
 
 
 
